d3890c82b577fd4d359504db93eed0fed8e51144
chetanchavan200210 <chetanchavan200210@gmail.com>
2025-09-25 11:47:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
     //Write your code here
-  
+  const radius = document.getElementById("radius");
+	
+	let btn=document.getElementById("submit")
+	let calc=(4/3) * Math.PI*Math.pow(radius,3);
+	btn.addEventListener('click',()=>{
+		const volume = document.getElemnetById("volume");
+		volume = calc;
+	})
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
